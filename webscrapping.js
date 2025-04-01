42e799197a06bc52a455ee6a8c9ed6e39ad55c45
Gabrielle Almeida <gabi.almeida2806@gmail.com>
2025-04-01 00:05:36 -0300

    Add files via upload

---
+++ webscrapping.js
@@ -0,0 +1,9 @@
+//deixa a barra lateral do youtube vermelhinha 
+
+function barraLateral() {
+    let barra = document.querySelector("ytd-mini-guide-renderer"); 
+    barra.style.backgroundColor = "red";
+     
+    }   
+
+barraLateral();
